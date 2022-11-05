d0cc7b791ec890ce5fe5b275022bbb88fc2c804e
John Albert <jpalbert81@gmail.com>
2022-11-05 16:31:24 -0700

    lambda test

--- helloDynamo.py
+++ helloDynamo.py
@@ -2,24 +2,33 @@
 import boto3
 from boto3.dynamodb.conditions import Key
 
-dynamoDb = boto3.resource('dynamodb')
+
+def db_test():
+    dynamoDb = boto3.resource('dynamodb')
+
+    tables = dynamoDb.tables.all()
+
+    for x in tables:
+        print(f"table : {x}")
+
+    fooTable = dynamoDb.Table('foo')
+    print(fooTable)
+
+    response = fooTable.query(KeyConditionExpression=Key('fookey').eq('98059'))
+
+    items = response['Items']
+    print(items)
+
+    scanResposne = fooTable.scan()
+    print(scanResposne)
 
 
-tables = dynamoDb.tables.all()
-
-for x in tables:
-    print(f"table : {x}")
-
-
-fooTable = dynamoDb.Table('foo')
-print(fooTable)
+def lambda_test():
+    client = boto3.client('lambda')
+    response = client.invoke(FunctionName='hello_lambda')
+    responseBody = response['Payload'].read()
+    print(responseBody)
 
 
-response = fooTable.query(KeyConditionExpression=Key('fookey').eq('98059'))
-
-items = response['Items']
-print(items)
-
-
-scanResposne = fooTable.scan()
-print(scanResposne)
+db_test()
+lambda_test()
